0904845c813bec47d1bf00beb3ba56842f66182c
ahladky <hladky.andriy@gmail.com>
2023-01-21 15:36:21 +0200

    first commit

---
+++ CSharp_Net_module1_4_1_lab/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Customer
+    {
+        // 6) declare private field name
+      private string name;
+
+        // 7) declare constructor to initialize name
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        // 8) declare method GotNewGoods with 2 parameters:
+        // 1 - object type
+        // 2 - GoodsInfoEventArgs type
+        public void GotNewGoods(object type, GoodsInfoEventArgs type2)
+        {
+
+        }
+
+    }
+}
---
+++ CSharp_Net_module1_4_1_lab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 9) declare object of OnlineShop 
+            OnlineShop on = new OnlineShop();
+            on.NewGoods("dfdff");
+            // 10) declare several objects of Customer
+            Customer c1 = new Customer("c1");
+            Customer c2 = new Customer("c2");
+            Customer c3 = new Customer("c3");
+            // 11) subscribe method GotNewGoods() of every Customer instance 
+            // to event NewGoodsInfo of object of OnlineShop
+          
+            // 12) invoke method NewGoods() of object of OnlineShop
+            // discuss results
+
+        }
+    }
+}
